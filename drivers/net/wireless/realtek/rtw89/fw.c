5469ea5a14aaad29b12b0e9f019c7d09ff855922
Gongwei Li <ligongwei@kylinos.cn>
2026-09-01 17:33:31 +0800

    wifi: rtw89: fw: propagate real error code in rtw89_fw_h2c_add_general_pkt()
    The error path of rtw89_fw_h2c_add_general_pkt() returns -ENOMEM
    unconditionally, which masks the real reason of a failure. In
    particular:
    
    * rtw89_fw_h2c_add_pkt_offload() can fail with -ENOSPC when no more
      packet offload slot is available, or with the error of H2C TX, but
      the callers only see -ENOMEM and get misled when debugging.
    
    * For an unsupported type, the switch default case returns -ENOMEM,
      which is misleading and should be -EINVAL.
    
    Return a real error code instead of -ENOMEM, and use -EINVAL for
    unsupported types.
    
    Signed-off-by: Gongwei Li <ligongwei@kylinos.cn>
    Acked-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260901093331.3441469-1-13875017792@163.com

--- drivers/net/wireless/realtek/rtw89/fw.c
+++ drivers/net/wireless/realtek/rtw89/fw.c
@@ -3091,11 +3091,14 @@ static int rtw89_fw_h2c_add_general_pkt(struct rtw89_dev *rtwdev,
 		skb = rtw89_arp_response_get(rtwdev, rtwvif_link);
 		break;
 	default:
+		ret = -EINVAL;
 		goto err;
 	}
 
-	if (!skb)
+	if (!skb) {
+		ret = -ENOMEM;
 		goto err;
+	}
 
 	ret = rtw89_fw_h2c_add_pkt_offload(rtwdev, &info->id, skb);
 	kfree_skb(skb);
@@ -3109,7 +3112,7 @@ static int rtw89_fw_h2c_add_general_pkt(struct rtw89_dev *rtwdev,
 
 err:
 	kfree(info);
-	return -ENOMEM;
+	return ret;
 }
 
 void rtw89_fw_release_general_pkt_list_vif(struct rtw89_dev *rtwdev,
